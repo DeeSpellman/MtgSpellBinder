4327b6ba5bb46afcabd054e90b4e34a9fcaa9050
Dee Spellman <thespellmanwitch@gmail.com>
2024-12-28 20:27:19 -0800

    Initial Commit

---
+++ MtgSpellBinderTests/MtgSpellBinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MtgSpellBinderTests.swift
+//  MtgSpellBinderTests
+//
+//  Created by Deirdre Spellman on 12/28/24.
+//
+
+import Testing
+@testable import MtgSpellBinder
+
+struct MtgSpellBinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
