c07760fdbf3604402733bab91a3a05ecaf1b81dd
Dee Spellman <thespellmanwitch@gmail.com>
2025-01-03 15:32:09 -0800

    updating models, starting to build add form for user

---
+++ AddNewCardView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewCardView.swift
+//  MtgSpellBinder
+//
+//  Created by Deirdre Spellman on 1/2/25.
+//
+
+import SwiftUI
+
+struct AddNewCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewCardView()
+}
